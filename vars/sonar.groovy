8e7037e1000aef51a2ff4f618bb530a0a2c0cc48
Davide <183734626+DavideP32@users.noreply.github.com>
2025-05-28 12:02:54 +0200

    .

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -28,12 +28,12 @@ def call(Map config = [:]) {
 
                 echo "Trovato pom.xml in: ${pomDir}"
                 dir (pomDir) {
-                   // sh "mvn clean verify sonar:sonar -Dsonar.projectKey=${projectKey} -Dsonar.login=${env.SONAR_TOKEN}"
+                   sh "mvn clean verify sonar:sonar -Dsonar.projectKey=${projectKey} -Dsonar.login=${env.SONAR_TOKEN}"
                 }
             } else {
                 echo "Nessun pom.xml trovato. Uso sonar-scanner CLI."
                 // Usare riga singola per evitare errori di shell
-                //sh "${env.SONAR_SCANNER}/bin/sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=. -Dsonar.login=${env.SONAR_TOKEN}"
+                sh "$env.SONAR_SCANNER/bin/sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=. -Dsonar.login=$env.SONAR_TOKEN"
             }
         }
     }
