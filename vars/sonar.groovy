f06ea60fcc4266b5d4005b36e9f048a0a984293c
Davide <183734626+DavideP32@users.noreply.github.com>
2025-05-28 10:46:37 +0200

    .

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -6,10 +6,13 @@ def call(Map config = [:]) {
         script {
             env.SONAR_SCANNER = tool 'SonarScanner'
             env.SONAR_TOKEN = credentials('jenkins-sonar')
-            
+
             if (!env.SONAR_SCANNER?.trim()) {
                 error "SonarScanner tool path is empty. Check Jenkins tool configuration."
             }
+
+            echo "DEBUG - SONAR_SCANNER path: ${env.SONAR_SCANNER}"
+            echo "DEBUG - PROJECT_KEY: ${projectKey}"
 
             // Trova pom.xml se presente
             def pomPath = sh(
@@ -26,15 +29,14 @@ def call(Map config = [:]) {
                 echo "Trovato pom.xml in: ${pomDir}"
                 dir(pomDir) {
                     sh """
-                        mvn clean verify sonar:sonar 
-                        -Dsonar.projectKey=${projectKey} 
-                        -Dsonar.login=$SONAR_TOKEN
+                        mvn clean verify sonar:sonar \
+                        -Dsonar.projectKey=${projectKey} \
+                        -Dsonar.login=${env.SONAR_TOKEN}
                     """
                 }
             } else {
                 echo "Nessun pom.xml trovato. Uso sonar-scanner CLI."
-                
-                
+                // Usare riga singola per evitare errori di shell
                 sh "${env.SONAR_SCANNER}/bin/sonar-scanner -Dsonar.projectKey=${projectKey} -Dsonar.sources=. -Dsonar.login=${env.SONAR_TOKEN}"
             }
         }
